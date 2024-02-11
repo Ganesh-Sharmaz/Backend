040d1fa3e7c5c741f87f4630acd281802a74ca71
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-11 14:09:53 +0530

    Frontend connected to backend via axios

--- Frontend01/src/App.jsx
+++ Frontend01/src/App.jsx
@@ -1,11 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
 
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get("/api/jokes")
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
+  
+
   return (
     <>
-      
+      <div className=' flex flex-col w-full h-screen bg-black justify-center items-center'>
+        <h1 className=' shadow-xl flex bg-slate-800 text-white px-5 py-5 text-center justify-center rounded-lg text-4xl font-bold'>Frontend is Ready</h1>
+        <p className=' shadow-xl bg-gray-500 px-2 py-2 rounded-xl mt-3 text-white'>Jokes: {jokes.length}</p>
+        <div className=' mt-4 shadow-xl bg-slate-600 rounded-lg px-5 py-5 text-white flex flex-col items-start'>
+        {
+          jokes.map((joke, index) => (
+            <div className='  w-full shadow-xl  px-5 py-5 text-white' key={joke.id}>
+              <h3 className=' font-bold text-lg'>{joke.title}</h3>
+              <p>{joke.content}</p>
+            </div>
+          ))
+        }
+        </div>
+
+      </div>
     </>
   )
 }
