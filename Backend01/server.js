9c366220b2cbd9b3c77d8ea37e36537cf6308255
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-11 12:59:44 +0530

    frontend added

---
+++ Backend01/server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('server is ready');
+// })
+
+app.get('/jokes',(req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'this is a joke'
+        },
+        {
+            id: 2,
+            title: 'Another joke',
+            content: 'this is another joke'
+        },
+        {
+            id: 3,
+            title: 'A third joke',
+            content: 'this is a third joke'
+        }
+    ];
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`serve at https://localhost:${port}`);
+})
+
