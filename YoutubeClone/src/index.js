5d23eeea40bc0eed840191b7f9ab055351c966db
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-17 00:55:06 +0530

    cors and cookies-parser installed

--- YoutubeClone/src/index.js
+++ YoutubeClone/src/index.js
@@ -7,6 +7,19 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.on("error", () => {
+        console.log("Error:", error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("mongoDB connection failed !! ", error);
+});
 
 
 
