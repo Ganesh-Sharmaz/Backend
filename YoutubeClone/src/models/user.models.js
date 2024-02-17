42cb78f9dd898315bf856b84b6d11bf765b49d55
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-18 01:17:13 +0530

    added cloudinary

--- YoutubeClone/src/models/user.models.js
+++ YoutubeClone/src/models/user.models.js
@@ -63,4 +63,31 @@ userSchema.methods.isPasswordCorrect = async function(){
     return await bycrpt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            fullName: this.fullName,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model("User", userSchema)
