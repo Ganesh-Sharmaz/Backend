e7824fb7ecf3975d3faaa26165c5284b2f57f7c2
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-24 01:07:29 +0530

    server chl gya

--- YoutubeClone/src/controllers/user.controller.js
+++ YoutubeClone/src/controllers/user.controller.js
@@ -1,17 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-<<<<<<< HEAD
     res.status(200).json(
         {
-            message:"ok"
+            message:"Chl rha hai server"
         }
     )
-=======
-    res.status(200).json({
-        message:"ok"
-    })
->>>>>>> bcc76cd621f61a8ee8006a5c5f24629b6fbbccd0
 })
 
 export {registerUser}
