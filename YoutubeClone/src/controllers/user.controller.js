92d681c1d2efed0dccccb44eb777480446ff084c
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-24 01:41:43 +0530

    update

--- YoutubeClone/src/controllers/user.controller.js
+++ YoutubeClone/src/controllers/user.controller.js
@@ -1,11 +1,18 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json(
-        {
-            message:"Chl rha hai server"
-        }
-    )
+    // get user details from front end
+    // validation - not empty
+    // check if user account already exist: username, email
+    // check for avatars, check for images
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token from feild from user
+    // check for user creation
+    // return res
+
+    const {fullName, username, email, password}= req.body
+    console.log("email: ", email);
 })
 
 export {registerUser}
