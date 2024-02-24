c6ddccb6e1dbca6d42d6a032bbdd93737a0f2293
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-25 01:44:52 +0530

    made auth middleware

--- YoutubeClone/src/controllers/user.controller.js
+++ YoutubeClone/src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { User } from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshToken = async(userId) {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+
+        return {refreshToken, accessToken}
+        
+    } catch (error) {
+        throw new ApiError(500, "something went wrong while generating access token and refresh token", error);
+        
+    }
+}
+
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from front end
     // validation - not empty
@@ -16,7 +33,7 @@ const registerUser = asyncHandler( async (req, res) => {
     // return res
 
     const {fullName, username, email, password}= req.body
-    console.log("email: ", email);
+    // console.log("email: ", email);
     
     if (
         [fullName, email, username, password].some((feild) => feild?.trim() === "")
@@ -36,9 +53,14 @@ const registerUser = asyncHandler( async (req, res) => {
     
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    console.log(avatarLocalPath)
-    console.log(req.files);
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // console.log(avatarLocalPath)
+    // console.log(req.files);
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file path is required");
@@ -70,6 +92,66 @@ const registerUser = asyncHandler( async (req, res) => {
     return res.status(201).json(
         new ApiResponse(200, createdUser, "user registered successfully")
     )
+
+})
+
+const   loginUser = asyncHandler( async (req, res) => {
+
+    // req body -> data
+    // usename or email
+    // find the user
+    // access and refresh token
+    // send cookies
+    // login user
+
+    const {email, username, password} = req.body
+
+    if (!email || !username) {
+        throw new ApiError(400, 'username or email is required');
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "user does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(404, "invalid user credentials")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokenAsync(user._id)
+
+    const loggedInUser = User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            
+            },
+            "user logged in successfully"
+            
+        )
+
+    )
+
+
+    const logOutUser
 
 })
 
