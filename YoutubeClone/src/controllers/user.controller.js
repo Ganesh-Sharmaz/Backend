e92e23ef50fe8eb9f74192a244e2cf720632a7b2
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-25 14:57:49 +0530

    logout user working fine and bug fixes

--- YoutubeClone/src/controllers/user.controller.js
+++ YoutubeClone/src/controllers/user.controller.js
@@ -181,8 +181,8 @@ const logOutUser = asyncHandler( async (req, res) => {
 
     return res
     .status(200)
-    .clearCookie("accessToken", accessToken, options)
-    .clearCookie("refreshToken", refreshToken, options)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, "User logged out succesfully"))
 
 })
